1363d462cd620b21ff693099479eda7778ae98e2
CodeFr33k <noreply>
2020-05-14 20:10:01 +0930

    vue create

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="sass">
+</style>
